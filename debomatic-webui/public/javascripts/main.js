545fc36da67c1c847a8fcb2a3819a94c2542fdb3
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-09 00:02:41 +0100

    start moving index to /distribution

--- debomatic-webui/public/javascripts/main.js
+++ debomatic-webui/public/javascripts/main.js
@@ -1,43 +1,48 @@
 var socket = io.connect('//localhost:3000');
-var old_data = Utils.from_hash_to_data()
 
-socket.on('distributions', function(distributions) {
-  Page_Distrubion.navbar.update(distributions)
-});
 
-socket.on('distribution_packages', function(data){
-  Page_Distrubion.packages.set(data)
-})
+if (window.location.pathname == '/') {
 
-socket.on('package_file_list', function(data){
-  Page_Distrubion.files.set(data)
-})
+  var old_data = Utils.from_hash_to_data()
 
-socket.on('file', function (data) {
-  Page_Distrubion.file.set(data)
-})
+  socket.on('distributions', function(distributions) {
+    Page_Distrubion.navbar.update(distributions)
+  });
 
-socket.on('file_newcontent', function(data) {
-  Page_Distrubion.file.append(data)
-})
+  socket.on('distribution_packages', function(data){
+    Page_Distrubion.packages.set(data)
+  })
+
+  socket.on('package_file_list', function(data){
+    Page_Distrubion.files.set(data)
+  })
+
+  socket.on('file', function (data) {
+    Page_Distrubion.file.set(data)
+  })
+
+  socket.on('file_newcontent', function(data) {
+    Page_Distrubion.file.append(data)
+  })
+
+  $(window).on('hashchange', function() {
+    data = Utils.from_hash_to_data()
+    Page_Distrubion.update(data, old_data)
+    old_data = data
+  });
+
+  $(window).on('load', function (){
+    Page_Distrubion.update(old_data)
+    $(window).scroll(function() {
+      var offset = $("#file").offset();
+      if ($(window).scrollTop() > offset.top + 220) {
+        $("#sticky").stop().addClass('fixed');
+      } 
+      else {
+        $("#sticky").stop().removeClass('fixed');
+      }
+    });
+  });
+}
 
 socket.on('error', function() { console.error(arguments) });
-
-$(window).on('hashchange', function() {
-  data = Utils.from_hash_to_data()
-  Page_Distrubion.update(data, old_data)
-  old_data = data
-});
-
-$(window).on('load', function (){
-  Page_Distrubion.update(old_data)
-  $(window).scroll(function() {
-    var offset = $("#file").offset();
-    if ($(window).scrollTop() > offset.top + 220) {
-      $("#sticky").stop().addClass('fixed');
-    } 
-    else {
-      $("#sticky").stop().removeClass('fixed');
-    }
-  });
-});
