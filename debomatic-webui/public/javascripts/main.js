c783290d7bfa60f4d1cbbbe8045e75d5bdd3a437
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-08 23:55:41 +0100

    removed unsed methods

--- debomatic-webui/public/javascripts/main.js
+++ debomatic-webui/public/javascripts/main.js
@@ -1,34 +1,3 @@
-function get_path(path) {
-  info = path.split('/');
-  data = {}
-  if (info.length >= 1) {
-    data.distribution = {}
-    data.distribution.name = info[0];
-    socket.emit("get_distribution_packages", data)
-    select_navbar_voice(data)
-  }
-  if (info.length >= 3){
-    data.package = {}
-    data.package.name = info[1];
-    data.package.version = info[2];
-    socket.emit("get_package_file_list", data)
-  }
-  if (info.length >= 4) {
-    data.file = {}
-    data.file.name = info[3]
-    socket.emit("get_file", data)
-  }
-  update_breadcrumb(path)
-}
-
-function select_navbar_voice(data) {
-
-}
-
-function update_breadcrumb(hash) {
-
-}
-
 var socket = io.connect('//localhost:3000');
 var old_data = Utils.from_hash_to_data()
 
