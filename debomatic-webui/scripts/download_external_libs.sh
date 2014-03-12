6e7364b19ab552f0ea7ede51f45c7027e22a6696
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-12 13:38:48 +0100

    get jquery 1.11.0 as externa_libs

--- debomatic-webui/scripts/download_external_libs.sh
+++ debomatic-webui/scripts/download_external_libs.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-TMP_DIR="`mktemp -d`"
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PUBLIC_DIR="${SCRIPTS_DIR}/../public/external_libs"
+EXT_LIBS_DIR="${SCRIPTS_DIR}/../public/external_libs"
 
 get_bootstrap() {
   VERSION="3.1.1"
   NAME="bootstrap-${VERSION}-dist"
-  if [ -d ${PUBLIC_DIR}/${NAME} ] ; then return ; fi
+  if [ -d ${EXT_LIBS_DIR}/${NAME} ] ; then return ; fi
   ARCHIVE=${NAME}.zip
   URL="https://github.com/twbs/bootstrap/releases/download/v${VERSION}/${ARCHIVE}"
-  cd $TMP_DIR
-  echo "Downloading bootstrap ..."
-  curl -s -O -L ${URL} &&
-  unzip -q ${ARCHIVE} && mv ${NAME} ${PUBLIC_DIR}
-  cd && rm -r $TMP_DIR
+  echo "Downloading bootstrap ${VERSION} ..."
+  curl -s -O -L ${URL} && \
+  unzip -q ${ARCHIVE} && rm ${ARCHIVE}
 }
 
-mkdir -p ${PUBLIC_DIR}
+get_jquery() {
+  VERSION="1.11.0"
+  DIR_JQUERY="jquery"
+  if [ -d ${EXT_LIBS_DIR}/${DIR_JQUERY} ] ; then return ; fi
+  mkdir ${DIR_JQUERY}
+  cd ${DIR_JQUERY}
+  URL="http://code.jquery.com/jquery-${VERSION}.min.js"
+  echo "Downloading jquery ${VERSION} ..."
+  curl -s -O -L ${URL}
+  cd ..
+}
+
+if [ ! -d ${EXT_LIBS_DIR} ] ; then mkdir -p ${EXT_LIBS_DIR} ; fi
+
+TMP_DIR="`mktemp -d`"
+cd ${TMP_DIR}
+
+get_jquery
 get_bootstrap
+
+if [ "`ls -1`" != "" ] ; then mv * ${EXT_LIBS_DIR} ; fi
+cd && rm -r ${TMP_DIR}
