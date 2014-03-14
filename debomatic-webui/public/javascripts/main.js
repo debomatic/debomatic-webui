2dab017e95dfdc5b3b3492f74f5fe08fdcc19641
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-14 14:33:01 +0100

    print on console.debug if exists - some usage inforation

--- debomatic-webui/public/javascripts/main.js
+++ debomatic-webui/public/javascripts/main.js
@@ -1,17 +1,30 @@
 var socket = io.connect('//' + config.hostname );
+/*
+  General indication for debugging use:
 
+  usage: debug(level, args)
+
+  args: can be everything
+
+  level: 1 - user interaction
+         2 - automatic changes
+         3 - socket interaction
+*/
 var debug = function() {
   if (arguments.length == 0) {
     return
   }
 	var level = 1
-	if (arguments.length > 1) {
+	if (arguments.length > 2) {
 		level = arguments[0]
-    arguments[0] = "dubug (" + level + ") > "
+    arguments[0] = "debug (" + level + ") > "
+    if (level <= config.preferences.debug) {
+      if (console.debug)
+        console.debug.apply(console, arguments)
+      else
+        console.log.apply(console, arguments)
+    }
   }
-	if (level <= config.preferences.debug) {
-    console.log.apply(console, arguments)
-	}
 }
 
 new Preferences()
