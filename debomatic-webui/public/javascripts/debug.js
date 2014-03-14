b9ee81a283b43cd82616ecdebdffaf6610c6f9b0
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-14 15:04:50 +0100

    moved debug functions to new file

---
+++ debomatic-webui/public/javascripts/debug.js
@@ -0,0 +1,43 @@
+/*
+  General indication for debugging use:
+
+  usage: debug(level, args)
+
+  args: can be everything
+
+  level: 1 - user interaction
+         2 - automatic changes
+         3 - socket emit data
+         4 - socket received data
+*/
+var debug = function() {
+  if (arguments.length == 0) {
+    return
+  }
+  var level = 1
+  if (arguments.length > 2) {
+    level = arguments[0]
+    arguments[0] = "debug (" + level + ") >"
+    if (level <= config.preferences.debug) {
+      if (console.debug)
+        console.debug.apply(console, arguments)
+      else
+        console.log.apply(console, arguments)
+    }
+  }
+}
+
+/*
+  An helper debug function for socket events
+
+  usage: debug_socket("emit"|"received", event_name, data)
+*/
+
+var debug_socket = function() {
+  if (arguments.length != 3)
+    return
+  var level = 3;
+  if (arguments[0] == "received")
+    level = 4
+  debug(level, "socket >", arguments[0], "event:", arguments[1], "data:", arguments[2])
+}
