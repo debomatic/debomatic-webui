d348e77354b3b9ad83ae8746af3901738cfb814a
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-14 14:00:22 +0100

    added debug function and setting

--- debomatic-webui/public/javascripts/main.js
+++ debomatic-webui/public/javascripts/main.js
@@ -1,5 +1,19 @@
 var socket = io.connect('//' + config.hostname );
 
+var debug = function() {
+  if (arguments.length == 0) {
+    return
+  }
+	var level = 1
+	if (arguments.length > 1) {
+		level = arguments[0]
+    arguments[0] = "dubug (" + level + ") > "
+  }
+	if (level <= config.preferences.debug) {
+    console.log.apply(console, arguments)
+	}
+}
+
 new Preferences()
 
 new Page_Generic().init(socket)
