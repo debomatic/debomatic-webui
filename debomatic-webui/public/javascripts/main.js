b2ec298eba71823d352c1cc7922dcc6883785185
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-14 14:50:56 +0100

    added debug helper function for socket

--- debomatic-webui/public/javascripts/main.js
+++ debomatic-webui/public/javascripts/main.js
@@ -18,7 +18,7 @@ var debug = function() {
 	var level = 1
 	if (arguments.length > 2) {
 		level = arguments[0]
-    arguments[0] = "debug (" + level + ") > "
+    arguments[0] = "debug (" + level + ") >"
     if (level <= config.preferences.debug) {
       if (console.debug)
         console.debug.apply(console, arguments)
@@ -26,6 +26,21 @@ var debug = function() {
         console.log.apply(console, arguments)
     }
   }
+}
+
+/*
+  An helper debug function for socket events
+
+  usage: socket_debug("emit"|"received", event_name, data)
+*/
+
+var socket_debug = function() {
+  if (arguments.length != 3)
+    return
+  var level = 3;
+  if (arguments[0] == "received")
+    level = 4
+  debug(level, "socket >", arguments[0], "event:", arguments[1], "data:", arguments[2])
 }
 
 new Preferences()
