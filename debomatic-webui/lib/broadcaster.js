4179b71cdc2dfe8c1de5095d0721ef7fa2678ec8
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-18 19:17:22 +0100

    fix appending more than one package with status building to status.packages - preventing user double upload same package and ui store two same entries with status.building

--- debomatic-webui/lib/broadcaster.js
+++ debomatic-webui/lib/broadcaster.js
@@ -14,24 +14,36 @@ function __watch_build_status (socket, status) {
       utils.errors_handler('Broadcaster:__watch_build_status:JSON.parse(new_content) - ', err, socket)
       return
     }
-    if (data.status == config.status.package.building) {
-      status.packages.push(data)
-    }
-    else if (data.status == config.status.package.sucessed
-      || data.status == config.status.package.failed )
+
+    // looking for same package already in status.packages
+    var index = -1
+    for(i = 0; i < status.packages.length; i++)
     {
-      for(i = 0; i < status.packages.length; i++)
+      p = status.packages[i]
+      if ( p.package == data.package
+        && p.distribution == data.distribution )
       {
-        p = status.packages[i]
-        if ( p.package == data.package
-          && p.distribution == data.distribution )
-        {
-          status.packages.splice(i, 1)
-          break
-        }
+        index = i
+        break
       }
     }
-    socket.emit(config.events.broadcast.status_update, data)
+
+    if (data.status == config.status.package.building) {
+      if (index == -1) { // not found in status.packages
+        status.packages.push(data)
+        socket.emit(config.events.broadcast.status_update, data)
+        return
+      }
+    }
+
+    if (data.status == config.status.package.successed
+      || data.status == config.status.package.failed ) 
+    {
+      if (index >= 0) { // found in status.packages - remove
+        status.packages.splice(index, 1)
+      }
+      socket.emit(config.events.broadcast.status_update, data)
+    }
   })
 }
 
