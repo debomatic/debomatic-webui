e8e27ece9b73fed584d7cb7b81c085f75067def4
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-19 22:52:37 +0100

    do not remove connect style - just use some custom style

---
+++ debomatic-webui/scripts/install/patch_css_directory_listing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CSS="${SCRIPTS_DIR}/../node_modules/express/node_modules/connect/lib/public/style.css"
+
+if [ "`grep debomatic-webui $CSS`" == "" ] ; then
+  
+  echo "Patching directory listing style.css"
+  cp $CSS $CSS.orig
+  echo "
+  /* debomatic-webui style patch */
+  ul#files li {
+    float: none;
+  }
+  /* end debomatic-webui patch */
+  " >> $CSS
+fi
+
--- debomatic-webui/scripts/install/remove_css_directory_listing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CSS="${SCRIPTS_DIR}/../node_modules/express/node_modules/connect/lib/public/style.css"
-
-if [ "`stat -c '%s' "$CSS"`" -gt 1 ] ; then
-  echo "Removing css from express directory listing ..."
-  echo "" > $CSS
-fi
