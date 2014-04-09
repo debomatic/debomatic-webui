385906bf9ce07a4044253368a3763e8baeecd571
Leo Iannacone <leo.iannacone@gmail.com>
2014-04-10 00:19:13 +0200

    start working on debomatic gui

---
+++ debomatic-gui/debomatic-gui/events.py
@@ -0,0 +1,34 @@
+from utils import json2obj
+from singleton import Singleton
+
+@Singleton
+class Events():
+    def __init__(self):
+        ## use scripts/get_events_json.js to get these
+        events = """
+        {
+            "error": "error",
+            "broadcast": {
+                "distributions": "distributions",
+                "status_update": "status_update"
+            },
+            "client": {
+                "distribution_packages": {
+                    "set": "distribution_packages",
+                    "get": "get_distribution_packages",
+                    "status": "package_status"
+                },
+                "package_files_list": {
+                    "set": "package_files_list",
+                    "get": "get_package_files_list"
+                },
+                "file": {
+                    "set": "file",
+                    "get": "get_file"
+                },
+                "file_newcontent": "file_newcontent",
+                "status": "status"
+            }
+        }
+        """
+        self.__dict__ = json2obj(events).__dict__
---
+++ debomatic-gui/debomatic-gui/utils.py
@@ -0,0 +1,36 @@
+from json import loads, dumps
+
+class dict2obj(object):
+    def __init__(self, d):
+        for key, value in d.items():
+            if isinstance(value, (list, tuple)):
+                setattr(self, key, [dict2obj(x) if \
+                    isinstance(x, dict) else x for x in value])
+            else:
+                setattr(self, key, dict2obj(value) \
+                    if isinstance(value, dict) else value)
+
+
+class json2obj(object):
+    def __init__(self, json_str):
+        json_dict = loads(json_str)
+        self.__dict__ = dict2obj(json_dict).__dict__
+
+
+def obj2dict(obj):
+    data = {}
+    for key, value in obj.__dict__.iteritems():
+        try:
+            if isinstance(value, (list, tuple)):
+                data[key] = [obj2dict(x) if \
+                    isinstance(x, object) else x for x in value]
+            else:
+                data[key] = obj2dict(value) \
+                    if isinstance(value, object) else value
+        except AttributeError:
+            data[key] = value
+    return data
+
+
+def obj2json(obj):
+    return dumps(obj2dict(obj))
