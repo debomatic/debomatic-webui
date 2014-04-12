40ebc12256e62d07d68292ff9ee5a566e2950a6e
Leo Iannacone <leo.iannacone@gmail.com>
2014-04-12 17:09:48 +0200

    show body and prototype some feature title

--- debomatic-gui/debomatic_gui/gtk/window.py
+++ debomatic-gui/debomatic_gui/gtk/window.py
@@ -1,6 +1,7 @@
 from gi.repository import Gtk
 from debomatic_gui.gtk.headerbar import HeaderBar
 from debomatic_gui.gtk.sidebar import Sidebar
+from debomatic_gui.gtk.body import Body
 
 class MyWindow(Gtk.Window):
 
@@ -9,20 +10,28 @@ class MyWindow(Gtk.Window):
         self.view = view
         # header bar
         self.headerbar = HeaderBar()
-        self.headerbar.set_title("Hello World")
+        self.headerbar.set_title("debomatic amd64")
+        self.headerbar.set_subtitle(view.host)
         self.set_titlebar(self.headerbar)
         self.view.attach_observer(self.headerbar)
-        self.set_size_request(400, 500)
+        self.set_size_request(800, 500)
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
         # sidebar
         sidebar = Sidebar(view)
+        hbox.add(sidebar)
 
-        scroll = Gtk.ScrolledWindow()
-        scroll.set_policy(Gtk.PolicyType.NEVER,
-                          Gtk.PolicyType.AUTOMATIC)
-        scroll.add(sidebar)
+        # separator
+        hbox.add(Gtk.Separator(orientation=\
+                Gtk.Orientation.VERTICAL))
 
-        self.add(scroll)
+        # body
+        body = Body()
+        self.view.attach_observer(body)
+        hbox.pack_start(body, True, True, 0)
+
+        self.add(hbox)
 
         self.connect('delete-event', self.on_delete)
 
@@ -36,6 +45,7 @@ class MyWindow(Gtk.Window):
 if __name__ == '__main__':
     from debomatic_gui.base.view import View
 
+    # view = View('debomatic-amd64.debian.net', 80)
     view = View('localhost')
     win = MyWindow(view)
     win.show_all()
