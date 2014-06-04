137646e5b593c67b5d58493801abb86005c06bc0
Leo Iannacone <l3on@ubuntu.com>
2014-06-04 15:35:48 +0200

    upgrade to express 4.x

--- debomatic-webui/scripts/install/patch_css_directory_listing.sh
+++ debomatic-webui/scripts/install/patch_css_directory_listing.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-CSS="${SCRIPTS_DIR}/../node_modules/express/node_modules/connect/node_modules/serve-index/public/style.css"
+CSS="${SCRIPTS_DIR}/../node_modules/serve-index/public/style.css"
 
 if [ "`grep debomatic-webui $CSS`" == "" ] ; then
-  
   echo "Patching directory listing style.css"
   cp $CSS $CSS.orig
   echo "
