43903d3a85fbd5802ed2bd3856afc0462596691b
Leo Iannacone <l3on@ubuntu.com>
2014-06-06 12:15:11 +0200

    Bumped version 0.2.3

--- debomatic-webui/lib/config.js
+++ debomatic-webui/lib/config.js
@@ -62,7 +62,7 @@ config.web.preferences.debug = 0; // debug level - 0 means disabled
 
 // DO NOT TOUCH these ones
 
-config.version = '0.2.2';
+config.version = '0.2.3';
 
 // A simple function to quickly have
 // get and set strings for client events
