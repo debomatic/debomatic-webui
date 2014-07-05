8ce190259b37f85080e65213d91b484a8311900d
Leo Iannacone <l3on@ubuntu.com>
2014-07-05 11:20:57 +0200

    pathing serve index css no longer needed

--- debomatic-webui/scripts/install.sh
+++ debomatic-webui/scripts/install.sh
@@ -2,7 +2,6 @@
 
 export SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bash ${SCRIPTS_DIR}/install/patch_css_directory_listing.sh
 bash ${SCRIPTS_DIR}/install/download_external_libs.sh
 
 python ${SCRIPTS_DIR}/install/create-user-config.py
