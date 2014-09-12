4727c50e285f7896231caad62a8a9e46d6f9c0dc
Leo Iannacone <l3on@ubuntu.com>
2014-09-12 12:43:52 +0200

    add download button to history - exports current view as csv file

--- debomatic-webui/public/javascripts/page_history.js
+++ debomatic-webui/public/javascripts/page_history.js
@@ -37,7 +37,7 @@ function Page_History() {
         var distribution_url = Utils.get_url_to_package({
             'distribution': p.distribution
         });
-        var row = '<tr id="' + _get_id(package_status) + '">';
+        var row = '<tr class="package" id="' + _get_id(package_status) + '">';
         var package_url = Utils.get_url_to_package(p);
         row += '<td><a href="' + distribution_url + '">' + p.distribution + '</a></td>';
         row += '<td><a href="' + package_url + '">' + p.package + '</td>';
@@ -143,6 +143,49 @@ function Page_History() {
         Chartist.Line('#days-chart', days_data);
     }
 
+    function _exportTableToCSV($table, filename) {
+        // code from http://jsfiddle.net/terryyounghk/KPEGU/
+        var $rows = $table.find('tr.package:visible:has(td)'),
+
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
+
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
+
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function (i, row) {
+                var $row = $(row),
+                    $cols = $row.find('td');
+
+                return $cols.map(function (j, col) {
+                    var $col = $(col),
+                        text = $col.text();
+
+                    return text.replace('"', '""'); // escape double quotes
+
+                }).get().join(tmpColDelim);
+
+            }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+            // Data URI
+            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+        $(this)
+            .attr({
+                'download': filename,
+                'href': csvData,
+                'target': '_blank'
+            });
+    }
+
+
+
     this.start = function (socket) {
         socket.on(config.events.broadcast.status_update, function (socket_data) {
             // TODO - implements _update_table
@@ -162,4 +205,8 @@ function Page_History() {
     _sort_table();
     _create_graph_distributions();
     _create_graph_days();
+
+    $('#download').on('click', function () {
+        _exportTableToCSV.apply(this, [$('#history'), 'history.csv']);
+    });
 }
