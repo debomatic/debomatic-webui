056a27510e0af56e3707217aae18ec4f6c205bd1
Leo Iannacone <l3on@ubuntu.com>
2014-09-15 18:04:18 +0200

    do not wrap package column

--- debomatic-webui/public/javascripts/page_history.js
+++ debomatic-webui/public/javascripts/page_history.js
@@ -39,12 +39,12 @@ function Page_History() {
         });
         var row = '<tr class="package" id="' + _get_id(package_status) + '">';
         var package_url = Utils.get_url_to_package(p);
-        row += '<td><a href="' + distribution_url + '">' + p.distribution + '</a></td>';
-        row += '<td><a href="' + package_url + '">' + p.package + '</td>';
-        row += '<td>' + Utils.format_time(p.start, false, true) + '</td>';
-        row += '<td>' + Utils.format_time(p.end, false, true) + '</td>';
-        row += '<td>' + p.uploader + '</td>';
-        row += '<td class="' + info.className + ' text-' + info.className + '">' + label + '</td>';
+        row += '<td class="distribution"><a href="' + distribution_url + '">' + p.distribution + '</a></td>';
+        row += '<td class="package"><a href="' + package_url + '">' + p.package + '</td>';
+        row += '<td class="start">' + Utils.format_time(p.start, false, true) + '</td>';
+        row += '<td class="end">' + Utils.format_time(p.end, false, true) + '</td>';
+        row += '<td class="uploader">' + p.uploader + '</td>';
+        row += '<td class="status ' + info.className + ' text-' + info.className + '">' + label + '</td>';
         row += '</tr>';
         $('table tbody').append(row);
     }
